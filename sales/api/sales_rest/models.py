7022ffd345bf18e84f5bdc3a759ea3e15e360877
Janete <martinezjanete8@gmail.com>
2022-10-24 15:58:36 -0500

    models & app registration

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=False)
+    import_href = models.CharField(max_length=200, unique=True)
+
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=200)
+    employee_number = models.PositiveSmallIntegerField(null=False)
+    picture_url = models.URLField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    name = models.Charfield(max_length=200)
+    address = models.CharField(max_length=400)
+    phone_number = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class SalesRecord(models.Model):
+    price = models.PositiveIntegerField(null=False)
+
+    customer = models.Foreignkey(
+        Customer,
+        related_name = "sales_records",
+        on_delete=models.CASCADE,
+    )
+
+    sales_person = models.ForeignKey(
+        SalesPerson,
+        related_name="sales_records",
+        on_delete=models.CASCADE,
+    )
+
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="sales_records",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.automobile.vin
