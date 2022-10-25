3d06beb2466a14e2dc3b607d3bccf5ea9e65008d
Ted <obrienteddy@hotmail.com>
2022-10-25 09:14:45 -0500

    Finished poller and views

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import AutomobileVO, Technician, ServiceAppointment
 
-# Create your views here.
+
+class AutomobileVO(ModelEncoder):
+    model = AutomobileVO
+    properties = [
+        "import_href",
+        "vin",
+    ]
+
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = [
+        "id",
+        "first_name",
+        "last_name",
+        "employee_number",
+        "employee_photo",
+    ]
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianEncoder,
+        )
